9d5a442eb133366d1135abf710ebdd2038527f39
Juancho Fantini <juan.fantini@ecloudsolutions.com>
2020-02-08 15:39:57 -0300

    Add: meal id to view model

--- app/src/main/java/com/bosqueoeste/ualapp/presentation/list/ItemMeal.kt
+++ app/src/main/java/com/bosqueoeste/ualapp/presentation/list/ItemMeal.kt
@@ -1,6 +1,7 @@
 package com.bosqueoeste.ualapp.presentation.list
 
 interface ItemMeal {
+    val id: String?
     val title: String
     val category: String
     val imageUrl: String?
--- app/src/main/java/com/bosqueoeste/ualapp/presentation/list/MealViewModel.kt
+++ app/src/main/java/com/bosqueoeste/ualapp/presentation/list/MealViewModel.kt
@@ -3,11 +3,13 @@ package com.bosqueoeste.ualapp.presentation.list
 import com.bosqueoeste.ualapp.data.models.Meal
 
 data class MealViewModel(
+    override val id: String?,
     override val title: String,
     override val category: String,
     override val imageUrl: String?
 ) : ItemMeal {
     constructor(meal: Meal) : this(
+        id = meal.idMeal,
         title = meal.strMeal.orEmpty(),
         category = meal.strCategory.orEmpty(),
         imageUrl = meal.strMealThumb

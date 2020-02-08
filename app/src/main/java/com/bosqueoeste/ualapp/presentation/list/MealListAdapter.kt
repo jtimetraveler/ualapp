965d18f37c03942713f0a7358dd3784e2ad449bc
Juancho Fantini <juan.fantini@ecloudsolutions.com>
2020-02-08 15:17:07 -0300

    Add: meal list adapter

---
+++ app/src/main/java/com/bosqueoeste/ualapp/presentation/list/MealListAdapter.kt
@@ -0,0 +1,54 @@
+package com.bosqueoeste.ualapp.presentation.list
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.bosqueoeste.ualapp.R
+import kotlinx.android.synthetic.main.item_meal.view.*
+
+class MealListAdapter(
+    private val onItemClickListener: (ItemMeal) -> Unit
+) :
+    androidx.recyclerview.widget.RecyclerView.Adapter<MealListAdapter.ViewHolder>() {
+
+    private var meals = listOf<ItemMeal>()
+
+    fun refreshData(newMeals: List<ItemMeal>) {
+        meals = newMeals
+        notifyDataSetChanged()
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view =
+            LayoutInflater.from(parent.context).inflate(R.layout.item_meal, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun getItemCount(): Int {
+        return meals.size
+    }
+
+
+    override fun onBindViewHolder(viewHolder: ViewHolder, position: Int) {
+        viewHolder.run {
+            meals[position].let { meal ->
+                onBind(meal)
+                itemView.setOnClickListener { onItemClickListener.invoke(meal) }
+            }
+        }
+    }
+
+    class ViewHolder(itemView: View) :
+        androidx.recyclerview.widget.RecyclerView.ViewHolder(itemView) {
+
+        fun onBind(meal: ItemMeal) {
+            itemView.run {
+                titleItemMeal.text = meal.title
+                categoryItemMeal.text = meal.category
+            }
+        }
+    }
+}
+
+
+

496aa2e4c5dbdd469931dfcefaf3a5d0e0598071
Juancho Fantini <juan.fantini@ecloudsolutions.com>
2020-02-08 13:05:35 -0300

    Add: empty project

---
+++ app/src/main/java/com/bosqueoeste/ualapp/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.bosqueoeste.ualapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}

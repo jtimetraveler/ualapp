b7be745e7024b94dff08c732f1325dfc682ed2b8
Juancho Fantini <juan.fantini@ecloudsolutions.com>
2020-02-08 14:02:30 -0300

    Add: meal list endpoint

--- app/src/main/java/com/bosqueoeste/ualapp/data/api/UalappApi.kt
+++ app/src/main/java/com/bosqueoeste/ualapp/data/api/UalappApi.kt
@@ -1,3 +1,14 @@
 package com.bosqueoeste.ualapp.data.api
 
-interface UalappApi
+import com.bosqueoeste.ualapp.data.api.response.MealListResponse
+import io.reactivex.Observable
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface UalappApi {
+
+    @GET("search.php")
+    fun getMeals(
+        @Query("s") search: String = ""
+    ): Observable<MealListResponse>
+}
---
+++ app/src/main/java/com/bosqueoeste/ualapp/data/api/response/MealListResponse.kt
@@ -0,0 +1,5 @@
+package com.bosqueoeste.ualapp.data.api.response
+
+import com.bosqueoeste.ualapp.data.models.Meal
+
+data class MealListResponse(val meals:List<Meal>)
